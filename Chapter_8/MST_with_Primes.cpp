6b4ad697adce1c317be560653088ee4e9f4785ae
Md. Abdul Hai Fahim <mahfahim51@gmail.com>
2024-11-02 10:34:24 +0600

    Update MST_with_Primes.cpp

--- Chapter_8/MST_with_Primes.cpp
+++ Chapter_8/MST_with_Primes.cpp
@@ -1,63 +1,5 @@
-// // MH_Nazmul
-// // Let it be as simple
 
-// #include <bits/stdc++.h>
-// using namespace std;
-// #define push_back push_back
-// #define nl cout << "\n"
-// #define all(x) x.begin(), x.end()
-// #define allr(x) x.rbegin(), x.rend()
-// #define f(a, b, c) for (int a = b; a < c; a++)
-// #define cin(vec, n) \
-//     f(i, 0, n) { cin >> vec[i]; }
-// #define int long long
-// const int N = 2e5 + 10;
-// const int M = 1e9 + 7;
-
-// void prims_cal(vector<vector<pair<int, int>>> &graph)
-// {
-//     priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
-
-//     vector<bool> vis(N, false);
-//     pair<int, int> zero = {0, 1};
-//     pq.push(zero);
-//     int node, weight, total_cost = 0;
-//     while (!pq.empty())
-//     {
-//         node = pq.top().second;
-//         weight = pq.top().first;
-//         pq.pop();
-//         if (vis[node] == true)
-//             continue;
-
-//         vis[node] = true;
-//         total_cost += weight;
-
-//         for (auto it : graph[node])
-//         {
-//             if (vis[it.second])
-//                 continue;
-//             pq.push({it.second, it.first});
-//         }
-//     }
-//     cout << total_cost;
-// }
-// int main();
-// {
-//     int node, edge;
-//     cin >> node >> edge;
-//     vector<vector<pair<int, int>>> graph(node + 5);
-//     while (edge--)
-//     {
-//         int v1, v2, w;
-//         cin >> v1 >> v2 >> w;
-//         graph[v1].push_back({v2, w});
-//         graph[v2].push_back({v1, w});
-//     }
-//     prims_cal(graph);
-//     return 0;
-// }
-
+// primes
 #include<bits/stdc++.h>
 using namespace std;
 
@@ -83,7 +25,11 @@ int  prims(int src)
        pair<int,int> papa = pq.top();
        int pnode = papa.first;
        int pcost = papa.second;
-       if(vis[pnode]==false) totalcost += pcost;
+       if(vis[pnode]==false) 
+       {
+        totalcost += pcost;
+        cout << pnode << " "<< pcost << endl;
+       }
        pq.pop();
        vis[pnode]=true;
        for(pair<int,int> child:mat[pnode])
@@ -113,6 +59,23 @@ int main()
     }
     memset(vis,false,sizeof(vis));
     int totalcost = prims(1);
-    cout << totalcost << endl;
+    // cout << totalcost << endl;
 
 }
+
+// input 
+
+// 4 5
+// 1 2 4
+// 1 3 8
+// 1 4 4
+// 2 3 3
+// 3 4 2
+
+// output
+
+// 1 0
+// 2 4
+// 3 3
+// 4 2
+
