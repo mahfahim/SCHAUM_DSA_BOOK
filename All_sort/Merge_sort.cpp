9a643a65a18acebe8542431e9256623ae20c8377
Md. Abdul Hai Fahim <mahfahim51@gmail.com>
2024-11-04 08:57:21 +0600

    Update Merge_sort.cpp

--- All_sort/Merge_sort.cpp
+++ All_sort/Merge_sort.cpp
@@ -1,84 +1,88 @@
-
-
-// Merge sort in C++
-#include <iostream>
-#include <vector>
+#include<bits/stdc++.h>
 using namespace std;
-
-// Merge two subarrays L and M into arr
-void merge(int arr[], int p, int q, int r) {
-  
-  // Create L ← A[p..q] and M ← A[q+1..r]
-  int n1 = q - p + 1;
-  int n2 = r - q;
-  
-  // Use std::vector to dynamically allocate arrays
-  vector<int> L(n1);
-  vector<int> M(n2);
-  
-  for (int i = 0; i < n1; i++)
-    L[i] = arr[p + i];
-    
-  for (int j = 0; j < n2; j++)
-    M[j] = arr[q + 1 + j];
-    
-  // Maintain current index of sub-arrays and main array
-  int i = 0, j = 0, k = p;
-  
-  // Until we reach either end of either L or M, pick larger among
-  // elements L and M and place them in the correct position at A[p..r]
-  while (i < n1 && j < n2) {
-    if (L[i] <= M[j]) {
-      arr[k] = L[i];
-      i++;
-    } else {
-      arr[k] = M[j];
-      j++;
+void merge(int a[],int l,int m,int r)
+{
+    int leftSize = m-l+1;
+    int rightSize = r-m;
+    int L[leftSize],R[rightSize];
+    int k=0;
+    for(int i=l;i<=m;i++)
+    {
+        L[k]=a[i];
+        k++;
     }
-    k++;
-  }
-  
-  // When we run out of elements in either L or M,
-  // pick up the remaining elements and put in A[p..r]
-  while (i < n1) {
-    arr[k] = L[i];
-    i++;
-    k++;
-  }
-  
-  while (j < n2) {
-    arr[k] = M[j];
-    j++;
-    k++;
-  }
-}
+    k=0;
+    for(int i=m+1;i<=r;i++)
+    {
+        R[k]=a[i];
+        k++;
+    }
+    int i=0,j=0;
+    int cur=l;
+    while(i<leftSize && j<rightSize)
+    {
+        if(L[i] <= R[j])
+        {
+            a[cur]=L[i];
+            i++;
+        }
+        else 
+        {
+            a[cur]=R[j];
+            j++;
+        }
+        cur++;
+    }
 
-// Divide the array into two subarrays, sort them and merge them
-void mergeSort(int arr[], int l, int r) {
-  if (l < r) {
-    // m is the point where the array is divided into two subarrays
-    int m = l + (r - l) / 2;
-    
-    mergeSort(arr, l, m);
-    mergeSort(arr, m + 1, r);
-    // Merge the sorted subarrays
-    merge(arr, l, m, r);
-  }
-}
+    while(i<leftSize)
+    {
+        a[cur]=L[i];
+        i++;
+        cur++;
+    }
 
-// Print the array
-void printArray(int arr[], int size) {
-  for (int i = 0; i < size; i++)
-    cout << arr[i] << " ";
-  cout << endl;
+    while(j<rightSize)
+    {
+        a[cur]=R[j];
+        j++;
+        cur++;
+    }
 }
+void merge_sort(int a[],int l,int r)
+{
+    if(l<r)
+    {
+        int mid=(l+r)/2;
+        merge_sort(a,l,mid);
+        merge_sort(a,mid+1,r);
+        merge(a,l,mid,r);
+        // cout<<"This"<<endl;
+        // for(int i=l;i<=mid;i++)
+        // {
+        //     cout<<a[i]<<" ";
+        // }
+        // cout<<endl;
+        // for(int i=mid+1;i<=r;i++)
+        // {
+        //     cout<<a[i]<<" ";
+        // }
+        // cout<<endl;
 
-// Driver program
-int main() {
-  int arr[] = {6, 5, 12, 10, 9, 1};
-  int size = sizeof(arr) / sizeof(arr[0]);
-  mergeSort(arr, 0, size - 1);
-  cout << "Sorted array: \n";
-  printArray(arr, size);
-  return 0;
+    }
+}
+int main()
+{
+    int n; 
+    cin>>n;
+    int a[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>a[i];
+    }
+    merge_sort(a,0,n-1);
+    // for(int i=0;i<n;i++)
+    // {
+    //     cout<<a[i]<<" ";
+    // }
+    return 0;
 }
